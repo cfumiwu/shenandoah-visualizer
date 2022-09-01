ba2e14e0547b9477d8a61d1150dda7f1bf680641
Celine Wu <cfumiwu@amazon.com>
2022-08-31 17:25:03 -0700

    Delete unused library

--- src/main/java/org/openjdk/shenandoah/ShenandoahVisualizer.java
+++ src/main/java/org/openjdk/shenandoah/ShenandoahVisualizer.java
@@ -28,7 +28,6 @@ import org.HdrHistogram.Histogram;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import javax.swing.plaf.synth.SynthTextPaneUI;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.IOException;
